d005988b36aad9003cc2d3f75f6111ebcb69d925
Cassy Cormier <93752385+prescottcassy@users.noreply.github.com>
2025-12-10 17:21:01 -0600

    Fix import statements formatting in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import './index.css'
-import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/Flyer_Generator">
     <App />
   </BrowserRouter>
 );
+
+
+
+
