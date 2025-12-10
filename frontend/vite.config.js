04cefba7295b00640f6033123182fac6e6cae2ea
Cassy Prescott <prescottcassy@gmail.com>
2025-12-10 03:03:10 -0600

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Flyer_Generator/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://zonal-cooperation-production.up.railway.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
